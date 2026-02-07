f3a7014a3cea23ef80c6af618c9e3d7e6c2b2c1b
IML-WKS-Admin <awacharshital94@gmail.com>
2026-02-07 17:31:42 -0500

    7 Feb 2025

---
+++ src/ex01_JavaBasics/e10DoWhile/e1.java
@@ -0,0 +1,14 @@
+package ex01_JavaBasics.e10DoWhile;
+
+public class e1 {
+    public static void main(String[] args) {
+//Print numbers from 1 to 10 using a do-while loop
+
+        int i = 1;
+        do {
+            System.out.println(i);
+            i++;
+        }while (i <= 10);
+    }
+
+}
