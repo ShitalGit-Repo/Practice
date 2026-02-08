ab3cff7c8ae85fcc44619f22cbed62761f2c64f5
IML-WKS-Admin <awacharshital94@gmail.com>
2026-02-08 17:30:46 -0500

    8 Feb 2025

---
+++ src/ex01_JavaBasics/e9WhileLoop/e1.java
@@ -0,0 +1,13 @@
+package ex01_JavaBasics.e9WhileLoop;
+
+public class e1 {
+    public static void main(String[] args) {
+//Print numbers from 1 to 10 using a while loop.
+
+        int i = 1;
+        while (i<11){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
