5b60041fc20c5b5d19c4ae4e10616c4f73f29739
IML-WKS-Admin <awacharshital94@gmail.com>
2026-02-11 17:29:57 -0500

    11 Feb 2025

---
+++ src/ex01_JavaBasics/e8ForLoop/e1.java
@@ -0,0 +1,14 @@
+package ex01_JavaBasics.e8ForLoop;
+
+public class e1 {
+    public static void main(String[] args) {
+//Q1. Print numbers from 1 to 10
+
+
+        for (int i = 1; i < 11; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
+
