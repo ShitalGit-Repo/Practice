3b2a17d0d8d84208e02b234c074db8fafc7913a8
IML-WKS-Admin <awacharshital94@gmail.com>
2026-02-17 09:49:47 -0500

    17 Feb 2025

---
+++ src/ex01_JavaBasics/e8ForLoop/e2.java
@@ -0,0 +1,13 @@
+package ex01_JavaBasics.e8ForLoop;
+
+public class e2 {
+    public static void main(String[] args) {
+//Q2. Print even numbers from 1 to 50
+
+        for (int i = 0; i < 50; i++) {
+            if (i%2 == 0){
+                System.out.println(i + "-Even no");
+            }
+        }
+    }
+}
