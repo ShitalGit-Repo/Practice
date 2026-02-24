9d38b755c2a86b016df83d0cfc8fcbe76892d918
IML-WKS-Admin <awacharshital94@gmail.com>
2026-02-23 22:56:24 -0500

    23 Feb 2025

---
+++ src/ex01_JavaBasics/e9WhileLoop/e5.java
@@ -0,0 +1,13 @@
+package ex01_JavaBasics.e9WhileLoop;
+
+public class e5 {
+    public static void main(String[] args) {
+//Print the multiplication table of 5 using a while loop.
+
+        int i = 1;
+        while (i <= 10){
+            System.out.println("5 X " + i + " = " + (5*i));
+            i++;
+        }
+    }
+}
