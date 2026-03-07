a55b1406366a0db37e3fcd8841a62dfeb20a3d88
IML-WKS-Admin <awacharshital94@gmail.com>
2026-03-07 18:09:44 -0500

    7th March 2026

---
+++ src/ex01_JavaBasics/e8ForLoop/e4.java
@@ -0,0 +1,11 @@
+package ex01_JavaBasics.e8ForLoop;
+
+public class e4 {
+    public static void main(String[] args) {
+//Q4. Print numbers from 10 to 1
+//👉 Use a reverse loop.
+        for (int i = 10; i >= 1 ; i--) {
+            System.out.println(i);
+        }
+    }
+}
