699e33b4f0e2935fddbfee7adcd8bc05d800aaac
IML-WKS-Admin <awacharshital94@gmail.com>
2026-03-08 18:08:20 -0500

    8th March 2026

---
+++ src/ex01_JavaBasics/e8ForLoop/e3.java
@@ -0,0 +1,14 @@
+package ex01_JavaBasics.e8ForLoop;
+
+public class e3 {
+    public static void main(String[] args) {
+//Q3. Print odd numbers from 1 to 20
+
+        for (int i = 0; i < 20; i++) {
+            if (i%2 != 0) {
+                System.out.println(i + "-odd no");
+            }
+
+        }
+    }
+}
