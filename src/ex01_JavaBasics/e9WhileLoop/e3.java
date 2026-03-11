b19d18a9498c73b248d21783800f2707590cdb9b
IML-WKS-Admin <awacharshital94@gmail.com>
2026-03-11 16:28:13 -0500

    11th March 2026

---
+++ src/ex01_JavaBasics/e9WhileLoop/e3.java
@@ -0,0 +1,12 @@
+package ex01_JavaBasics.e9WhileLoop;
+
+public class e3 {
+    public static void main(String[] args) {
+//Print numbers from 10 to 1 (reverse order).
+        int i = 10;
+        while (i > 0) {
+            System.out.println(i);
+            i--;
+        }
+    }
+}
