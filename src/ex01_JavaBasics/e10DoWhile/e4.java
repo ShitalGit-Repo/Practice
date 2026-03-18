a1926410756ed2f812fcc72c6a9a5a8bf57b4143
IML-WKS-Admin <awacharshital94@gmail.com>
2026-03-18 16:37:54 -0500

    18th March 2026

---
+++ src/ex01_JavaBasics/e10DoWhile/e4.java
@@ -0,0 +1,13 @@
+package ex01_JavaBasics.e10DoWhile;
+
+public class e4 {
+    public static void main(String[] args) {
+//Display "Hello Java" 5 times using a do-while loop.
+
+        int i = 1;
+        do {
+            System.out.println("Hello Java");
+            i++;
+        }while (i<=5) ;
+    }
+}
