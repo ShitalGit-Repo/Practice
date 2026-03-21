2ac47b5ab029dd8fe75a8fc7416d02c3dab9d7de
IML-WKS-Admin <awacharshital94@gmail.com>
2026-03-20 22:09:04 -0500

    20th March 2026

---
+++ src/ex01_JavaBasics/e12Array/e2.java
@@ -0,0 +1,20 @@
+package ex01_JavaBasics.e12Array;
+
+import java.util.Scanner;
+
+public class e2 {
+    public static void main(String[] args) {
+//Q - Right Angle Triangle
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the n : ");
+        int n = scanner.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
