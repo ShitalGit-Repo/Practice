0aa1664ae75cc2e6193b4f48396cea02be5b7929
IML-WKS-Admin <awacharshital94@gmail.com>
2026-03-24 22:14:04 -0500

    24 March 2026

---
+++ src/ex01_JavaBasics/e15Inheritance/e1SingleInheritandMethodCall/Cat.java
@@ -0,0 +1,10 @@
+package ex01_JavaBasics.e15Inheritance.e1SingleInheritandMethodCall;
+
+public class Cat extends animal{
+
+    void meow(){
+        System.out.println("Derived class - Meow");
+    }
+
+    }
+
