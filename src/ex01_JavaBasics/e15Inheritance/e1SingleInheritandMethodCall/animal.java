ada7012d6a073cafb01fc40adf33a6a739d9342b
IML-WKS-Admin <awacharshital94@gmail.com>
2026-03-25 22:15:06 -0500

    25 March 2026

---
+++ src/ex01_JavaBasics/e15Inheritance/e1SingleInheritandMethodCall/animal.java
@@ -0,0 +1,10 @@
+package ex01_JavaBasics.e15Inheritance.e1SingleInheritandMethodCall;
+
+public class animal {
+
+    void makesound() {
+        System.out.println("Animal class - MakeSound");
+    }
+}
+
+
